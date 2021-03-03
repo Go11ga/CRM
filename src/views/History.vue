7e30c14343effb47632762f4c5c7db351acfc119
Igor Khromov <go11ga@rambler.ru>
2021-03-03 10:03:39 +0300

    feat: history chart

--- src/views/History.vue
+++ src/views/History.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="history-chart">
-      <canvas></canvas>
+      <canvas ref="canvas"></canvas>
     </div>
 
     <Loader v-if="loading" />
@@ -38,9 +38,11 @@
 <script>
 import HistoryTable from '@/components/HistoryTable'
 import paginationMixin from '@/mixins/pagination.mixin'
+import { Pie } from 'vue-chartjs'
 
 export default {
   name: 'history',
+  extends: Pie,
   components: {
     HistoryTable
   },
@@ -53,17 +55,54 @@ export default {
     this.records = await this.$store.dispatch('fetchRecords')
     const categories = await this.$store.dispatch('fetchCategories')
 
-    this.setupPagination(this.records.map(record => {
-      return {
-        ...record,
-        categoryName: categories.find(c => c.id === record.categoryId).title,
-        typeClass: record.type === 'income' ? 'green' : 'red',
-        typeText: record.type === 'income' ? 'Доход' : 'Расход',
-
-      }
-    }))
+    this.setup(categories)
 
     this.loading = false
+  },
+  methods: {
+    setup (categories) {
+      this.setupPagination(this.records.map(record => {
+        return {
+          ...record,
+          categoryName: categories.find(c => c.id === record.categoryId).title,
+          typeClass: record.type === 'income' ? 'green' : 'red',
+          typeText: record.type === 'income' ? 'Доход' : 'Расход',
+
+        }
+      }))
+
+      this.renderChart({
+        labels: categories.map(el => el.title),
+        datasets: [{
+            label: 'Расходы по категориям',
+            data: categories.map(el => {
+              return this.records.reduce((total, r) => {
+                if (r.categoryId === el.id && r.type === 'outcome') {
+                  total += +r.amount
+                }
+                return total
+              }, 0)
+            }),
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)',
+              'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+      })
+    }
   }
 }
 </script>
